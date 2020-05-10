6092caa9ed1c6e8209ddc1ef4ddb02f4279ff0d0
markgit666 <943121438@qq.com>
2020-05-10 10:24:43 +0800

    main.js添加axios配置

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import global from './views/Global'
+import axios from 'axios'
+
 // 挂载到Vue实例上面
 import 'jquery'
 import Validate from './assets/lib/rules.js'// 引用文件
 Vue.prototype.GLOBAL = global
 // import $ from 'jquery'
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+axios.defaults.withCredentials =true;
 // import { $axios } from './assets/lib/util.js'
 // Vue.prototype.$http = $axios
 // Vue.use($)
